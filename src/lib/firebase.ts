f884c2c4929bd9647c76132a006d2f9bfe244f38
Victor Mbuya <victormbuya2@gmail.com>
2025-07-24 06:51:12 +0000

    no changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 
 // Import the functions you need from the SDKs you need
+import 'dotenv/config';
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
