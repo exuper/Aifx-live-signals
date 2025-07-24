517c7271936c139061d4fd3abc32681ebc7e0ae2
Victor Mbuya <victormbuya2@gmail.com>
2025-07-24 06:32:18 +0000

    fix this error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,4 @@
 
-import { config } from 'dotenv';
-config({ path: '.env.local' });
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -16,7 +13,6 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
